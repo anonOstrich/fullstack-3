6f01c849026a70b49f8a5e0926baf63a2f6c6c78
anonOstrich <jesper.kuutti@helsinki.fi>
2019-01-22 19:12:40 +0200

    Tehty ->3.8]

--- index.js
+++ index.js
@@ -3,7 +3,12 @@ const bodyParser = require('body-parser');
 const morgan = require('morgan'); 
 const app = express(); 
 app.use(bodyParser.json());
-app.use(morgan('tiny')); 
+
+
+morgan.token('body', (req) => (JSON.stringify(req.body)))
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time'
++ ' ms :body')); 
 
 
 let notes = [
