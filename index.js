bc1ef5bbbb12823b43fa31a6c73869006caa24f0
anonOstrich <jesper.kuutti@helsinki.fi>
2019-01-22 18:47:43 +0200

    Tehty -> 3.7]

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express'); 
 const bodyParser = require('body-parser'); 
+const morgan = require('morgan'); 
 const app = express(); 
 app.use(bodyParser.json());
+app.use(morgan('tiny')); 
 
 
 let notes = [
