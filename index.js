801dec16541fbf0fac1e23793f8abf76cbd0a161
anonOstrich <jesper.kuutti@helsinki.fi>
2019-01-22 17:54:26 +0200

    Tehty ->3.1

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require('express'); 
+const bodyParser = require('body-parser'); 
+const app = express(); 
+app.use(bodyParser.json());
+
+
+const notes = [
+    {
+    id: 1, 
+    name: "Arto Hellas", 
+    number: "045-1236543"
+    }, 
+
+    {
+        id: 2, 
+        name: "Arto Järvinen", 
+        number: "041-21423123"
+    }, 
+    {
+        id: 3, 
+        name: "Lea Kutvonen", 
+        number: "040-4323234"
+    }, 
+    {
+        id: 4, 
+        name: "Martti Tienari", 
+        number: "09-784232"
+    }
+]
+
+app.get("/api/persons", (request, response) => {
+    response.json(notes); 
+})
+
+
+
+
+const PORT = 3001; 
+app.listen(PORT, () => {
+    console.log(`Listeting to port number ${PORT}`)
+}); 
