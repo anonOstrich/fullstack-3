0121b2b630eee083e9547b4459acff08c0574e49
Jesper Kuutti <jesper@Jesper-Air.home>
2019-01-24 17:27:08 +0200

    Fixed a comma error in frontend's package.json

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.39f618a49f32c23bf383293405658558.js"
+  "/precache-manifest.a30e27b8634b8c302c32adaddd064076.js"
 );
 
 workbox.clientsClaim();
