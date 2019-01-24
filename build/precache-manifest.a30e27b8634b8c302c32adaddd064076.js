1a9bacc7047655e715d1aa41c9cbbe518295d6f7
Jesper Kuutti <jesper@Jesper-Air.home>
2019-01-24 17:32:02 +0200

    Fixed a comma error in frontend's package.json

--- build/precache-manifest.a30e27b8634b8c302c32adaddd064076.js
+++ build/precache-manifest.a30e27b8634b8c302c32adaddd064076.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/static/js/runtime~main.229c360f.js"
   },
   {
-    "revision": "319f1ee03ae16fccb3b6",
-    "url": "/static/js/main.319f1ee0.chunk.js"
+    "revision": "3abe5c7cc3e648301a00",
+    "url": "/static/js/main.3abe5c7c.chunk.js"
   },
   {
     "revision": "23feea3aedfdbea1a84a",
     "url": "/static/js/1.23feea3a.chunk.js"
   },
   {
-    "revision": "cf8cf5efaf80394e4159aa3a422a62de",
+    "revision": "01e30f3850cca61d67d936bcbc5b1c39",
     "url": "/index.html"
   }
 ];
